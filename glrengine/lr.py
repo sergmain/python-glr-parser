b774c3b2d9ad14b33309b5e5361b3fc418b92f96
Marat Khabibullin <maratfm@gmail.com>
2016-05-26 22:33:44 -0700

    print table

--- glrengine/lr.py
+++ glrengine/lr.py
@@ -1,43 +1,20 @@
 # -*- coding: utf-8 -*-
 from collections import defaultdict, OrderedDict
 from collections import namedtuple
-from itertools import ifilter, imap, izip
 
-from glrengine import GLRScanner
 from glrengine.rules import RuleSet
 
 Item = namedtuple('Item', ['rule_index', 'dot_position'])
 
 
-def expand_item(item, R):
-    rule = R[item[0]]
-    return rule[1], item[1], rule[0]
-
-
-def expand_itemset(itemset, R):
-    return imap(lambda x: expand_item(x, R), itemset)
-
-
-def expand_itemset2(itemset, R):
-    for item in itemset:
-        rule = R[item[0]]
-        yield item[0], rule[1], item[1], rule[0]
-
-
-def itemstr(item, R):
-    e, i, n = expand_item(item, R)
-    return ("[%s -> %s . %s" % (n, ' '.join(e[:i]), ' '.join(e[i:]))).strip() + ']'
-
-
-def itemsetstr(itemset, R, label=''):
-    items = map(lambda x: itemstr(x, R), sorted(itemset))
-    width = reduce(lambda a, b: max(a, len(b)), items, 3)
-    label = label and '[' + unicode(label) + ']' or ''
-    build = ["+-%s%s-+" % (label, '-' * (width - len(label)))]
-    build.extend("| %-*s |" % (width, item) for item in items)
-    build.append("+-" + "-" * width + '-+')
-    return '\n'.join(build)
-
+def itemstr():
+    pass
+def itemsetstr():
+    pass
+def first():
+    pass
+def kernel():
+    pass
 
 def iterate_lookaheads(itemset, rules):
     for item in itemset:
@@ -52,20 +29,11 @@ def iterate_lookaheads(itemset, rules):
         yield item, lookahead
 
 
-def first(itemset, rules):
-    """
-        Set of the tokens at the right of each dot in this item set
-    """
-    result = set()
-    for item, lookahead in iterate_lookaheads(itemset, rules):
-        if not lookahead in rules:
-            result.add(lookahead)
-    return result
-
 def unique(seq):
     seen = set()
     seen_add = seen.add
     return [x for x in seq if not (x in seen or seen_add(x))]
+
 
 def follow(itemset, rules):
     """
@@ -109,6 +77,7 @@ def closure(itemset, rules):
 
 Node = namedtuple('Node', ['index', 'itemset', 'follow_dict', 'parent_rule_index', 'parent_lookahead'])
 Action = namedtuple('Action', ['action', 'rule_index'])
+
 
 def generate_graph(rules):
     assert isinstance(rules, RuleSet)
@@ -213,149 +182,3 @@ def generate_tables(rules):
 
         result.append(actions)
     return result
-
-
-
-def old(rules):
-    nodes = []
-    node_by_itemset = {}
-
-    first_node = Node(0, closure([Item(0, 0)], rules), {})
-
-    LR0 = set()
-    x = closure([Item(0, 0)], rules)
-    stack = [tuple(sorted(x))]
-    while stack:
-        x = stack.pop()
-        LR0.add(x)
-        F = follow(x, rules)
-        for t, s in F.iteritems():
-            s = tuple(sorted(s))
-            if s not in LR0:
-                stack.append(s)
-    LR0 = list(sorted(LR0))
-
-    LR0_idx = {}
-    for i, s in enumerate(LR0):
-        LR0_idx[s] = i
-
-    GOTO = []
-    for s in LR0:
-        f = {}
-        for tok, dest in follow(s, rules).iteritems():
-            f[tok] = LR0_idx[tuple(sorted(closure(dest, rules)))]
-        GOTO.append(f)
-
-
-    ACTION = []
-    for s, g in izip(LR0, GOTO):
-        action = defaultdict(list)
-
-        # свертки
-        for item in ifilter(lambda item: item.dot_position == len(rules[item.rule_index].elements), s):
-            if not item.rule_index:
-                action['$'].append(('A', 0))
-            # else:
-            #     for kw in self.following_tokens(item):
-            #         action[kw].append(('R', item.rule_index))
-
-        # переносы
-        for tok, dest in g.iteritems():
-            action[tok].append(('S', dest))
-
-        # commit
-        ACTION.append(action)
-
-    return ACTION, GOTO, LR0
-
-def kernel(itemset):
-    """
-        The kernel items in this item set
-    """
-    return set(ifilter(lambda (r, i): not r or i, itemset))
-
-
-
-dictionaries = {
-    u"VARIABLES": [u"A", u"B", u"C"]
-}
-
-grammar = u"""
-S = Sums
-Sums = Sums 'plus' Products
-Sums = Products
-Products = Products 'mul' Value
-Products = Value
-Value = number
-Value = VARIABLES
-"""
-
-class GrammarParser(object):
-    DEFAULT_PARSER = {
-        "word": r"[\w\d_-]+",
-        "number": r"[\d]+",
-        "space": r"[\s]+",
-        "newline": r"[\n]+",
-        "dot": r"[\.]+",
-        "comma": r"[,]+",
-        "colon": r"[:]+",
-        "percent": r"[%]+",
-        "quote": r"[\"\'«»`]+",
-        "brace": r"[\(\)\{\}\[\]]+",
-    }
-
-    DEFAULT_PARSER_DISCARD_NAMES = ["space"]
-
-    DEFAULT_GRAMMAR = """
-        Word = word
-        Word = noun
-        Word = adj
-        Word = verb
-        Word = pr
-        Word = dpr
-        Word = num
-        Word = adv
-        Word = pnoun
-        Word = prep
-        Word = conj
-        Word = prcl
-        Word = lat
-    """
-
-    def parse_grammar(self, grammar, dictionaries):
-        grammar_rules = u"%s\n%s" % (grammar, self.DEFAULT_GRAMMAR)
-        if dictionaries:
-            # превращает {k: [a, b, c]} -> "k = 'a' | 'b' | 'c'"
-            for dict_name, dict_words in dictionaries.items():
-                morphed = []
-                for word in dict_words:
-                    # morphed.append(morph_parser.normal(word))
-                    morphed.append(word)
-                grammar_rules += u"\n%s = '%s'" % (dict_name, "' | '".join(morphed))
-
-        parser_rules = self.DEFAULT_PARSER
-        parser_rules.update({"discard_names": self.DEFAULT_PARSER_DISCARD_NAMES})
-        return parser_rules
-
-parser_rules = GrammarParser().parse_grammar(grammar, dictionaries)
-
-# for k,v in parser_rules.items():
-#     print k , v
-
-scanner = GLRScanner(**parser_rules)
-rules = RuleSet(grammar, set(scanner.tokens.keys()).union({'$'}), 'S')
-
-nodes = generate_graph(rules)
-t = generate_tables(rules)
-
-symbols = sorted(unique(k for row in t for k in row.keys()))
-print '\n # |',
-for k in symbols:
-    print '%-9s|' % k,
-for i, row in enumerate(t):
-    print '\n%2d |' % i,
-    for k in symbols:
-        print '%-9s|' % (', '.join('%s%s' % a for a in row[k]) if k in row else ''),
-
-folowers = generate_followers(rules)
-print folowers
--- run.py
+++ run.py
@@ -1,4 +1,129 @@
 # coding=utf-8
+import sys
+
 from glrengine import GLRScanner
 from glrengine.lr import *
 
+
+
+
+dictionaries = {
+    u"VARIABLES": [u"A", u"B", u"C"]
+}
+
+grammar = u"""
+S = Sums
+Sums = Sums 'plus' Products
+Sums = Products
+Products = Products 'mul' Value
+Products = Value
+Value = number
+Value = VARIABLES
+"""
+
+
+grammar = u"""
+S = Sums
+Sums = Sums 'plus' Sums
+Sums = number
+S = E
+E = A '1'
+E = B '2'
+A = '1'
+B = '1'
+"""
+
+class GrammarParser(object):
+    DEFAULT_PARSER = {
+        "word": r"[\w\d_-]+",
+        "number": r"[\d]+",
+        "space": r"[\s]+",
+        "newline": r"[\n]+",
+        "dot": r"[\.]+",
+        "comma": r"[,]+",
+        "colon": r"[:]+",
+        "percent": r"[%]+",
+        "quote": r"[\"\'«»`]+",
+        "brace": r"[\(\)\{\}\[\]]+",
+    }
+
+    DEFAULT_PARSER_DISCARD_NAMES = ["space"]
+
+    DEFAULT_GRAMMAR = """
+        Word = word
+        Word = noun
+        Word = adj
+        Word = verb
+        Word = pr
+        Word = dpr
+        Word = num
+        Word = adv
+        Word = pnoun
+        Word = prep
+        Word = conj
+        Word = prcl
+        Word = lat
+    """
+
+    def parse_grammar(self, grammar, dictionaries):
+        grammar_rules = u"%s\n%s" % (grammar, self.DEFAULT_GRAMMAR)
+        if dictionaries:
+            # превращает {k: [a, b, c]} -> "k = 'a' | 'b' | 'c'"
+            for dict_name, dict_words in dictionaries.items():
+                morphed = []
+                for word in dict_words:
+                    # morphed.append(morph_parser.normal(word))
+                    morphed.append(word)
+                grammar_rules += u"\n%s = '%s'" % (dict_name, "' | '".join(morphed))
+
+        parser_rules = self.DEFAULT_PARSER
+        parser_rules.update({"discard_names": self.DEFAULT_PARSER_DISCARD_NAMES})
+        return parser_rules
+
+parser_rules = GrammarParser().parse_grammar(grammar, dictionaries)
+
+# for k,v in parser_rules.items():
+#     print k , v
+
+scanner = GLRScanner(**parser_rules)
+rules = RuleSet(grammar, set(scanner.tokens.keys()).union({'$'}), 'S')
+
+nodes = generate_graph(rules)
+t = generate_tables(rules)
+
+
+def print_table(table, buf):
+    col_widths = [0] * len(table[0])
+    for row in table:
+        for j, cell in enumerate(row):
+            col_widths[j] = max(col_widths[j], len(str(cell)))
+    print col_widths
+
+
+    for i, row in enumerate(table):
+        for j, cell in enumerate(row):
+            buf.write(str(cell).ljust(col_widths[j]))
+            buf.write(' | ')
+        buf.write('\n')
+
+table = []
+symbols = sorted(unique(k for row in t for k in row.keys()))
+table.append([''] + symbols)
+for i, row in enumerate(t):
+    res = [i]
+    for k in symbols:
+        res.append(', '.join('%s%s' % a for a in row[k]) if k in row else '')
+    table.append(res)
+
+print_table(table, sys.stdout)
+
+# print '\n # |',
+# for k in symbols:
+#     print '%-9s|' % k,
+# for i, row in enumerate(t):
+#     print '\n%2d |' % i,
+#     for k in symbols:
+#         print '%-9s|' % (', '.join('%s%s' % a for a in row[k]) if k in row else ''),
+
+folowers = generate_followers(rules)
+print folowers
