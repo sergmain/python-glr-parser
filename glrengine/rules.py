ed849c563938d657a3b6e9a4305a02a514e39c25
Marat Khabibullin <maratfm@gmail.com>
2016-06-07 14:24:40 -0700

    copy to old location

---
+++ glrengine/rules.py
@@ -0,0 +1,161 @@
+# coding=utf8
+from collections import defaultdict, namedtuple
+from glrengine.scanner import make_scanner
+
+Rule = namedtuple('Rule', ['name', 'elements', 'commit'])
+
+class RuleSet(dict):
+    lr_grammar_scanner = make_scanner(
+        sep='=',
+        alt='[|]',
+        word=r"\b\w+\b",
+        raw=r"\'[^\s']+\'",
+        whitespace=r'[ \t\r\n]+',
+        minus=r'[-]',
+        label=r'\<.+?\>',
+        discard_names=('whitespace',)
+    )
+
+    def __init__(self, grammar, kw_set, start_sym='S'):
+        dict.__init__(self)
+        rules = self.__make_rules(grammar, kw_set, start_sym)
+        self.names_count = 0
+        self.rules_count = 0
+        self.labels = {}
+        self.__init(rules)
+
+    def __make_rules(self, grammar, kw, start):
+        words = [start]
+        labels = []
+        edit_rule = '@'
+        edit_rule_commit = True
+        next_edit_rule_commit = True
+        kw.add(edit_rule)
+        for tokname, tokvalue, tokpos in self.lr_grammar_scanner(grammar):
+            if tokname == 'minus':
+                next_edit_rule_commit = False
+            if tokname == 'word' or tokname == 'raw':
+                words.append(tokvalue)
+                labels.append(None)
+                kw.add(tokvalue)
+            elif tokname == 'alt':
+                yield (edit_rule, tuple(words), edit_rule_commit, labels[1:-1])
+                words = []
+                labels = []
+            elif tokname == 'sep':
+                tmp = words.pop()
+                yield (edit_rule, tuple(words), edit_rule_commit, labels[1:-1])
+                edit_rule_commit = next_edit_rule_commit
+                next_edit_rule_commit = True
+                edit_rule = tmp
+                words = []
+                labels = [None]
+            elif tokname == 'label':
+                # "a=b, b=c, d" -> {"a": "b", "b": "c", "d": None}
+                tokvalue = tokvalue.strip().replace(" ", "")
+                label = defaultdict(list)
+                for l in tokvalue[1:-1].split(","):
+                    key, value = tuple(l.split("=", 1) + [None])[:2]
+                    label[key].append(value)
+                # label = dict([tuple(l.split("=", 1) + [None])[:2] for l in tokvalue[1:-1].split(",")])
+                labels[-1] = label
+        yield (edit_rule, tuple(words), edit_rule_commit, labels[1:-1])
+
+    def __init(self, rules):
+        epsilons = self.__fill(rules)
+        must_cleanup = False
+        while epsilons:
+            eps = epsilons.pop()
+            if eps in self:
+                # Rule produces something and has an epsilon alternative
+                self.__add_epsilon_free(eps, epsilons)
+            else:
+                must_cleanup |= self.__remove_epsilon(eps, epsilons)
+        if must_cleanup:
+            rules = sorted(self[i] for i in xrange(self.rules_count) if self[i] is not None)
+            epsilons = self.__fill(rules)
+            if epsilons:
+                #print "D'oh ! I left epsilon rules in there !", epsilons
+                raise Exception("There is a bug ! There is a bug ! " +
+                                "Failed to refactor this grammar into " +
+                                "an epsilon-free one !")
+
+    def __fill(self, rules):
+        self.names_count = 0
+        self.rules_count = 0
+        self.clear()
+        epsilons = set()
+        for rulename, elems, commit, labels in rules:
+            if len(elems) > 0:
+                self.__add(rulename, elems, commit, labels)
+            else:
+                epsilons.add(rulename)
+        #print 'found epsilon rules', epsilons
+        return epsilons
+
+    def __add(self, rulename, elems, commit, labels):
+        if rulename not in self:
+            self.names_count += 1
+            self[rulename] = set()
+        rule = Rule(rulename, elems, commit)
+        if rule not in (self[i] for i in self[rulename]):
+            self[rulename].add(self.rules_count)
+            self[self.rules_count] = rule
+            self.labels[self.rules_count] = labels
+            self.rules_count += 1
+
+    def __add_epsilon_free(self, eps, epsilons):
+        #print "Adding", eps, "-free variants"
+        i = 0
+        while i < self.rules_count:
+            if self[i] is None:
+                i += 1
+                continue
+            rulename, elems, commit = self[i]
+            if eps in elems:
+                #print "... to", rulename, elems
+                E = set([elems])
+                old = 0
+                while len(E) != old:
+                    old = len(E)
+                    E = E.union(elems[:i] + elems[i + 1:]
+                                for elems in E
+                                for i in xrange(len(elems))
+                                if elems[i] == eps)
+                #print "Created variants", E
+                for elems in E:
+                    if len(elems) == 0:
+                        #print "got new epsilon rule", rulename
+                        epsilons.add(rulename)
+                    else:
+                        self.__add(rulename, elems, commit, [])
+                        #
+                        #
+            i += 1
+            #
+
+    def __remove_epsilon(self, eps, epsilons):
+        must_cleanup = False
+        i = 0
+        while i < self.rules_count:
+            if self[i] is None:
+                i += 1
+                continue
+            rulename, elems, commit = self[i]
+            if eps in elems:
+                elems = tuple(e for e in elems if e != eps)
+                if len(elems) == 0:
+                    # yet another epsilon :/
+                    self[i] = None
+                    self[rulename].remove(i)
+                    if not self[rulename]:
+                        del self[rulename]
+                    must_cleanup = True
+                    epsilons.add(rulename)
+                    #print "epsilon removal created new epsilon rule", rulename
+                else:
+                    self[i] = Rule(rulename, elems, commit)
+                    #
+            i += 1
+        return must_cleanup
+
---
+++ glrengine/utils.py
@@ -0,0 +1,129 @@
+# coding=utf-8
+from glrengine.lr import unique, Action
+
+
+def print_table(table, buf):
+    col_widths = [0] * len(table[0])
+    for row in table:
+        for j, cell in enumerate(row):
+            col_widths[j] = max(col_widths[j], len(str(cell)))
+
+    def print_row(i, chars, row=None):
+        if i > 0 and i % 2 == 0:
+            buf.write('\033[48;5;236m')
+        for j, cell in enumerate(row or col_widths):
+            if j == 0:
+                buf.write(chars[0:2])
+
+            if row:
+                buf.write(str(cell).ljust(col_widths[j]))
+            else:
+                buf.write(chars[1] * col_widths[j])
+
+            if j < len(col_widths) - 1:
+                buf.write(chars[1:4])
+            else:
+                buf.write(chars[3:5])
+
+        buf.write('\033[m')
+        buf.write('\n')
+
+    for i, row in enumerate(table):
+        if i == 0:
+            print_row(i, u'┌─┬─┐')
+        elif i == 1:
+            print_row(i, u'├─┼─┤')
+        if True:
+            print_row(i, u'│ │ │', row)
+        if i == len(table) - 1:
+            print_row(i, u'└─┴─┘')
+
+
+def gen_printable_table(action_table):
+    table = []
+    symbols = unique(k for row in action_table for k in row.keys())
+    symbols = sorted(symbols, key=lambda x: (x[0].isupper(), x))
+    table.append([''] + symbols)
+    for i, row in enumerate(action_table):
+        res = [i]
+        for k in symbols:
+            res.append(', '.join('%s%s%s' % (a,s or '',r or '') for a,s,r in row[k]) if k in row else '')
+        table.append(res)
+    return table
+
+
+def print_stack_item(stack_item, second_line_prefix=''):
+
+    def get_pathes(stack_item):
+        if stack_item.prev:
+            for prev in stack_item.prev:
+                for p in get_pathes(prev):
+                    yield p + [stack_item]
+        else:
+            yield [stack_item]
+
+    if stack_item.prev:
+        pathes = []
+        for path in get_pathes(stack_item):
+            pathes.append(' > '.join(repr(i) for i in path))
+        length = max(len(p) for p in pathes)
+
+        results = []
+        for i, path in enumerate(pathes):
+            result = '' if i == 0 else second_line_prefix
+            result += path.rjust(length)
+            if len(pathes) > 1:
+                if i == 0:
+                    result += ' ╮'
+                elif i != len(pathes) - 1:
+                    result += ' │'
+                else:
+                    result += ' ╯'
+            results.append(result)
+
+        return '\n'.join(results)
+    else:
+        return '0'
+
+
+def gen_ast(node, last=False, prefix = ''):
+    line = prefix[:-1]
+    if prefix:
+        if not last:
+            line += u'├──'
+        else:
+            line += u'╰──'
+    else:
+        line = '  '
+    line += node.token.symbol
+    yield line, node.token.value
+
+    if node.reduced:
+        for i, r in enumerate(node.reduced):
+            last = i==len(node.reduced)-1
+            for line, value in gen_ast(r, last, prefix + ('   ' if last else u'  │')):
+                yield line, value
+
+
+def print_ast(node):
+    ast = list(gen_ast(node))
+    depth = max(len(l) for l, v in ast)
+    for l, v in ast:
+        print l.ljust(depth,u'.' if v else u' '), v
+
+
+def change_state_indexes(table, mapping):
+    '''
+    Keeps table logically same, but updates rule indexes by switching mapping pairs
+    :param table:
+    :param mapping:
+    :return:
+    '''
+    reverse_map = dict((v,k) for k,v in mapping.items())
+    result = [table[reverse_map.get(i,i)] for i in range(len(table))]
+    for row in result:
+        for symbol, actions in row.items():
+            for i, action in enumerate(actions):
+                if action.state in mapping:
+                    actions[i] = Action(action.type, mapping[action.state], action.rule_index)
+    return result
